6069ea73a4ad09630e917b4d024d94b6bf6db557
LemoZun <marrzun@gmail.com>
2024-07-18 14:40:03 +0900

    Update Program.cs

--- MySecondGit/Program.cs
+++ MySecondGit/Program.cs
@@ -24,7 +24,12 @@
             Item Sword;
             Monster ork;
             ork.hp = 100;
+<<<<<<< HEAD
+            ork.mp = 300;
+
+=======
             ork.mp = 50;
+>>>>>>> ade5bb86f42311be4511b5acc340228302b31068
 
         }
     }    
